e7091b9e625900a626c62d00af97acbe404c6485
Dean Jenkins <skullandbones@example.com>
2023-10-30 18:51:14 +0000

    u2d: Add v0.1 source code plus U2D.COM and U2D.SUB
    U2D.COM is v0.1 which does a minimal UNIX to DOS conversion.
    
    On CP/M 2.2, build u2d.c by executing the U2D.SUB script.
    J>C:SUBMIT U2D
    
    Signed-off-by: Dean Jenkins <skullandbones@example.com>

--- u2d.c
+++ u2d.c
@@ -6,31 +6,59 @@
 
 #include <stdio.h>
 
-int main(void)
+char *filename_in = NULL;
+char *filename_out = "NEWDOS.TXT";
+
+int main(int argc, char *argv[])
 {
-    FILE *fp;
+    FILE *fp_in;
+    FILE *fp_out;
     char buf[2];
-    unsigned int count = 0;
 
     printf("U2D.COM a UNIX2DOS clone\n");
+    printf("Written by Dean Jenkins v0.1 29/10;2023\n");
+    printf("u2d <input filename>\n");
 
-    fp = fopen("TEST.TXT","rb");
-    
-    if (!fp)
+    if (argc != 2)
     {
-         printf("File failed to open\n");   
+         printf("Please specify an input filename\n");   
          exit(1);
     }
 
-    printf("File was opened\n");
+    filename_in = argv[1];
 
-    while (fgets(buf, 2, fp) != NULL)
+    fp_in = fopen(filename_in, "rb");
+
+    if (!fp_in)
     {
-        count++;
-        printf("%4u: %c\n", count, buf[0]);
-    
-    
+         printf("File '%s' failed to open\n", filename_in);   
+         exit(1);
     }
 
-    fclose(fp);
+    printf("Input file '%s' was opened\n", filename_in);
+
+    fp_out = fopen(filename_out, "wb");
+
+    if (!fp_out)
+    {
+         printf("File '%s' failed to open\n", filename_out);   
+         close(fp_in);
+         exit(1);
+    }
+
+    printf("Output file '%s' was opened\n", filename_out);
+
+    while (fgets(buf, 2, fp_in) != NULL)
+    {
+        if (buf[0] == 0x1A) /* EOF */
+            break;
+
+        if (buf[0] == 0x0A) /* LF */
+            fputc(0x0D, fp_out); /* Inject as CR */
+
+        fputc(buf[0], fp_out); /* Write to new file */
+    }
+
+    fclose(fp_in);
+    fclose(fp_out);
 }
